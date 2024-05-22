acf643cedf67ef4866ef07105f584f16e12a1236
Ayush Raj <119657235+rajayush01@users.noreply.github.com>
2024-05-23 01:51:05 +0530

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
         loop
         className="absolute top-0 left-0 h-full w-full z-[1] object-cover"
       >
-        <source src="/bg1.webm" type="video/webm" />
+        <source src="/bg1 (1).webm" type="video/webm" />
       </video>
       <div className="relative z-[2] flex flex-col h-full w-full items-center justify-center bg-opacity-20 bg-black text-white">
         <HeroContent/>
